a77744fe5b599b2676580febdf0f8a8ae85614c0
Pirokar <Pirokar13@yandex.ru>
2013-08-21 15:07:05 +0400

    init commit

---
+++ Assets/Scripts/plank_move_h.js
@@ -0,0 +1,10 @@
+﻿var x = 0;
+var a = 1;
+function LateUpdate () {
+	if (x<=100) {
+		transform.position = transform.position + a*(new Vector3 (0, 0.05, 0));
+		x = x + a*1;
+	}
+	if (x==100|x==0)
+		a*=-1;
+}
