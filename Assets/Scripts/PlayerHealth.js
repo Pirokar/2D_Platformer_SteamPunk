292a0337298cd0b7d1b5a406dd6a4bcb726f0f46
Pirokar <Pirokar13@yandex.ru>
2013-08-21 16:11:23 +0400

    init commit

--- Assets/Scripts/PlayerHealth.js
+++ Assets/Scripts/PlayerHealth.js
@@ -1,9 +1,28 @@
-﻿#pragma strict
-
-function Start () {
-
-}
+﻿var maxHealth : int = 100;
+var currentHealth : int = 100;
+var bool : boolean = false;
 
 function Update () {
+	if(currentHealth < 0) {
+		currentHealth = 0;
+	}
+	if(currentHealth > maxHealth) {
+		currentHealth = maxHealth;
+	}
+	if(maxHealth < 1) {
+		maxHealth = 1;
+	}
+	if(currentHealth < 0) {
+		Application.LoadLevel ("GameOver");
+	}
+}
 
+function OnGUI() {
+	if(GUI.Button (Rect(120,10,100,20), "Status")){
+		bool = !bool;
+	}
+	if(bool){
+		GUI.Box (Rect(120,30,100,50),"");
+		GUI.Label (Rect(120,30,100,50), "Health: "+currentHealth);
+	}
 }
