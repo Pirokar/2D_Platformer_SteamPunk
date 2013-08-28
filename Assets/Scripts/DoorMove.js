b4925d9f81d77822bc4cf14a023c97b502b16edf
Pirokar <Pirokar13@yandex.ru>
2013-08-28 23:14:35 +0400

    init commit

---
+++ Assets/Scripts/DoorMove.js
@@ -0,0 +1,14 @@
+﻿static var open : boolean = false;
+private var x : int = 0;
+var MovingLength : int = 300;
+
+function Update() {
+	if(open) {
+		if (x<=MovingLength) {
+			transform.position +=(Vector3(0, 1, 0));
+			x += 1;
+		}
+		else
+			open = false;
+	}
+}
---
+++ Assets/Scripts/EnteringZone.js
@@ -0,0 +1,30 @@
+﻿static var steamsToOpen : int = 2;
+static var done : boolean = false;
+static var needSteams : boolean = false;
+static var isBack : boolean = false;
+
+function OnTriggerEnter(player : Collider) {
+	if(!done)
+		if(!isBack && player.tag=="Player")
+			if(SteamSystem.currentSteams<steamsToOpen)
+				needSteams = true;
+			else {
+				Application.LoadLevelAdditive("FirstLogic");
+				Time.timeScale = 0;
+			}
+}
+
+function OnTriggerExit(player : Collider) {
+	if(player.tag=="Player") {
+		needSteams = false;
+		isBack = false;
+	}
+}
+
+function OnGUI() {
+	if(needSteams) {
+		GUI.Box(Rect(Screen.width/2-100,Screen.height/2-50,200,100),"");
+		GUI.Label(Rect(Screen.width/2-80,Screen.height/2-40,160,80),"Нужно собрать 30 шестеренок, чтобы задействовать механизм");
+	}
+}
+	
