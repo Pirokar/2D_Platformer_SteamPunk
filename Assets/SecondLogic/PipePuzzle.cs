9175a7d33fba70123bfdfd6f117170c7ee9a689d
latnedis <bulat.9407@gmail.com>
2013-09-01 16:46:25 +0400

    logic2

---
+++ Assets/SecondLogic/PipePuzzle.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipePuzzle : MonoBehaviour
+{
+	public PipePuzzleBlock prefab;
+	public GameObject startBlockPrefab;
+	public GameObject endBlockPrefab;
+	public Material straightMat;
+	public Material cornerMat;
+	public Material crossMat;
+	public PipePuzzleBlock[] blocks;
+	private Transform pipesParent;
+	private int start, end;
+	public int size;
+	public bool[] water;
+	
+	#region Singletone
+	private static PipePuzzle _instance;
+
+	public static PipePuzzle Instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType (typeof(PipePuzzle)) as PipePuzzle;
+			}
+			return _instance;
+		}
+	}
+	#endregion
+
+	// Use this for initialization
+	void Start ()
+	{
+		GeneratePuzzle (8);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	void GeneratePuzzle (int fieldSize)
+	{
+		size = fieldSize;
+		// Preparings
+		pipesParent = (new GameObject ("_pipes")).transform;
+		blocks = new PipePuzzleBlock[size * size];
+		water = new bool[size * size];
+		
+		//Start/end
+		start = Random.Range (0, size);
+		end = Random.Range (0, size);
+		GameObject startBlock = Instantiate (startBlockPrefab) as GameObject;
+		GameObject endBlock = Instantiate (endBlockPrefab) as GameObject;
+		startBlock.transform.parent = pipesParent;
+		endBlock.transform.parent = pipesParent;
+		startBlock.transform.position = new Vector3 (start, size + 1, 0f);
+		endBlock.transform.position = new Vector3 (end, 0, 0f);
+		
+		// Create Objects
+		for (int j = 0; j < size; j++) {
+			for (int i = 0; i < size; i++) {
+				int index = j * size + i;
+				blocks [index] = (Instantiate (prefab.gameObject) as GameObject).GetComponent<PipePuzzleBlock> ();
+				blocks [index].transform.parent = pipesParent;
+				blocks [index].name = "Block_" + i + "_" + j;
+				blocks [index].myI = i;
+				blocks [index].myJ = j;
+				blocks [index].transform.position = new Vector3 (i, size - j, 0f);
+				blocks [index].MyType = (Random.value < 0.3f) ? (Random.value > 0.5f ? PipeType.Corner : PipeType.Cross) : PipeType.Straight;
+			}
+		}
+		
+		// Generate types
+		
+		// Center to camera
+		pipesParent.transform.position = new Vector3 (-size * 0.5f, -size * 0.5f, 0f);
+	}
+	
+	public bool Check ()
+	{
+		for (int i = 0; i < blocks.Length; i++) {
+			blocks [i].renderer.material.color = Color.white;
+
+		}
+		
+		water = new bool[size * size];
+		FillPipeRecursively (blocks [start]);
+		if (water [size * (size - 1) + end]) {
+			Debug.Log ("WIN!!!");
+		}
+		return water [size * (size - 1) + end];
+	}
+	
+	public void FillPipeRecursively (PipePuzzleBlock block)
+	{
+		int i = block.myI;
+		int j = block.myJ;
+		
+		int index = j * size + i;
+		water [index] = true;
+		block.renderer.material.color = Color.blue;
+		
+		FillSafely (block, Side.Up, Side.Bottom, (j - 1) * size + i);
+		FillSafely (block, Side.Bottom, Side.Up, (j + 1) * size + i);
+		FillSafely (block, Side.Right, Side.Left, j * size + i + 1);
+		FillSafely (block, Side.Left, Side.Right, j * size + i - 1);
+	}
+	
+	public void FillSafely (PipePuzzleBlock block, Side side, Side neighbourSide, int neighbourBlockIndex)
+	{
+		if (block.IsSideAccessible (side)) {
+			PipePuzzleBlock neighbourBlock = blocks [neighbourBlockIndex];
+			if (!water [neighbourBlockIndex]) {
+				if (neighbourBlock.IsSideAccessible (neighbourSide)) {
+					FillPipeRecursively (neighbourBlock);
+				}
+			}
+		}
+	}
+}
--- Assets/SecondLogic/win2.cs
+++ Assets/SecondLogic/win2.cs
@@ -1,7 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class win2 : MonoBehaviour {
+public class win2 : MonoBehaviour
+{
+	
+	#region Singletone
+	private static win2 _instance;
+
+	public static win2 Instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.Find("_puzzle").GetComponent<win2>();
+			}
+			return _instance;
+		}
+	}
+	#endregion
+	
 	public Transform p09;
 	public Transform p08;
 	public Transform p18;
@@ -59,39 +74,40 @@ public class win2 : MonoBehaviour {
 	public Transform p90;
 	
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		win2._instance = this;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if ((p09.rotation==Quaternion.Euler(0,0,0)||p09.rotation==Quaternion.Euler(0,0,180))&&p08.rotation==Quaternion.Euler(0,0,270)&&
-			p18.rotation==Quaternion.Euler(0,0,180)&&(p17.rotation==Quaternion.Euler(0,0,0)||p17.rotation==Quaternion.Euler(0,0,180))&&
-			p14.rotation==Quaternion.Euler(0,0,0)&&p04.rotation==Quaternion.Euler(0,0,270)&&p05.rotation==Quaternion.Euler(0,0,180)&&
-			p25.rotation==Quaternion.Euler (0,0,0)&&p26.rotation==Quaternion.Euler (0,0,180)&&(p36.rotation==Quaternion.Euler (0,0,0)||
-			p36.rotation==Quaternion.Euler (0,0,180))&&p46.rotation==Quaternion.Euler (0,0,0)&&(p48.rotation==Quaternion.Euler (0,0,0)||
-			p48.rotation==Quaternion.Euler (0,0,180))&&p49.rotation==Quaternion.Euler (0,0,180)&&p39.rotation==Quaternion.Euler (0,0,180)&&
-			(p38.rotation==Quaternion.Euler (0,0,0)||p38.rotation==Quaternion.Euler (0,0,180))&&p37.rotation==Quaternion.Euler (0,0,270)&&
-			(p57.rotation==Quaternion.Euler (0,0,0)||p57.rotation==Quaternion.Euler (0,0,180))&&p67.rotation==Quaternion.Euler (0,0,0)&&
-			(p68.rotation==Quaternion.Euler (0,0,0)||p68.rotation==Quaternion.Euler (0,0,180))&&p69.rotation==Quaternion.Euler (0,0,180)&&
-			p79.rotation==Quaternion.Euler (0,0,180)&&(p78.rotation==Quaternion.Euler (0,0,0)||p78.rotation==Quaternion.Euler (0,0,180))&&
-			p77.rotation==Quaternion.Euler (0,0,270)&&p87.rotation==Quaternion.Euler (0,0,180)&&(p86.rotation==Quaternion.Euler (0,0,0)||
-			p86.rotation==Quaternion.Euler (0,0,180))&&(p85.rotation==Quaternion.Euler (0,0,0)||p85.rotation==Quaternion.Euler (0,0,180))&&
-			p84.rotation==Quaternion.Euler (0,0,270)&&p94.rotation==Quaternion.Euler (0,0,180)&&(p93.rotation==Quaternion.Euler (0,0,0)||
-			p93.rotation==Quaternion.Euler (0,0,180))&&p92.rotation==Quaternion.Euler (0,0,0)&&(p82.rotation==Quaternion.Euler (0,0,0)||
-			p82.rotation==Quaternion.Euler (0,0,180))&&(p62.rotation==Quaternion.Euler (0,0,0)||p62.rotation==Quaternion.Euler (0,0,180))&&
-			(p52.rotation==Quaternion.Euler (0,0,0)||p52.rotation==Quaternion.Euler (0,0,180))&&p22.rotation==Quaternion.Euler (0,0,270)&&
-			p23.rotation==Quaternion.Euler (0,0,180)&&p53.rotation==Quaternion.Euler (0,0,0)&&(p54.rotation==Quaternion.Euler (0,0,0)||
-			p54.rotation==Quaternion.Euler (0,0,180))&&p55.rotation==Quaternion.Euler (0,0,180)&&(p65.rotation==Quaternion.Euler (0,0,0)||
-			p65.rotation==Quaternion.Euler (0,0,180))&&p75.rotation==Quaternion.Euler (0,0,180)&&(p74.rotation==Quaternion.Euler (0,0,0)||
-			p74.rotation==Quaternion.Euler (0,0,180))&&(p73.rotation==Quaternion.Euler (0,0,0)||p73.rotation==Quaternion.Euler (0,0,180))&&
-			p71.rotation==Quaternion.Euler (0,0,0)&&(p61.rotation==Quaternion.Euler (0,0,0)||p61.rotation==Quaternion.Euler (0,0,180))&&
-			(p51.rotation==Quaternion.Euler (0,0,0)||p51.rotation==Quaternion.Euler (0,0,180))&&p41.rotation==Quaternion.Euler (0,0,270)&&
-			p44.rotation==Quaternion.Euler (0,0,180)&&p34.rotation==Quaternion.Euler (0,0,180)&&(p31.rotation==Quaternion.Euler (0,0,0)||
-			p31.rotation==Quaternion.Euler (0,0,180))&&p30.rotation==Quaternion.Euler (0,0,270)&&(p50.rotation==Quaternion.Euler (0,0,0)||
-			p50.rotation==Quaternion.Euler (0,0,180))&&(p70.rotation==Quaternion.Euler (0,0,0)||p70.rotation==Quaternion.Euler (0,0,180))&&
-			(p80.rotation==Quaternion.Euler (0,0,0)||p80.rotation==Quaternion.Euler (0,0,180))&&p90.rotation==Quaternion.Euler (0,0,180)){
-			Debug.Log("win");
+	public void Check ()
+	{
+		if ((p09.rotation == Quaternion.Euler (0, 0, 0) || p09.rotation == Quaternion.Euler (0, 0, 180)) && p08.rotation == Quaternion.Euler (0, 0, 270) &&
+			p18.rotation == Quaternion.Euler (0, 0, 180) && (p17.rotation == Quaternion.Euler (0, 0, 0) || p17.rotation == Quaternion.Euler (0, 0, 180)) &&
+			p14.rotation == Quaternion.Euler (0, 0, 0) && p04.rotation == Quaternion.Euler (0, 0, 270) && p05.rotation == Quaternion.Euler (0, 0, 180) &&
+			p25.rotation == Quaternion.Euler (0, 0, 0) && p26.rotation == Quaternion.Euler (0, 0, 180) && (p36.rotation == Quaternion.Euler (0, 0, 0) ||
+			p36.rotation == Quaternion.Euler (0, 0, 180)) && p46.rotation == Quaternion.Euler (0, 0, 0) && (p48.rotation == Quaternion.Euler (0, 0, 0) ||
+			p48.rotation == Quaternion.Euler (0, 0, 180)) && p49.rotation == Quaternion.Euler (0, 0, 180) && p39.rotation == Quaternion.Euler (0, 0, 180) &&
+			(p38.rotation == Quaternion.Euler (0, 0, 0) || p38.rotation == Quaternion.Euler (0, 0, 180)) && p37.rotation == Quaternion.Euler (0, 0, 270) &&
+			(p57.rotation == Quaternion.Euler (0, 0, 0) || p57.rotation == Quaternion.Euler (0, 0, 180)) && p67.rotation == Quaternion.Euler (0, 0, 0) &&
+			(p68.rotation == Quaternion.Euler (0, 0, 0) || p68.rotation == Quaternion.Euler (0, 0, 180)) && p69.rotation == Quaternion.Euler (0, 0, 180) &&
+			p79.rotation == Quaternion.Euler (0, 0, 180) && (p78.rotation == Quaternion.Euler (0, 0, 0) || p78.rotation == Quaternion.Euler (0, 0, 180)) &&
+			p77.rotation == Quaternion.Euler (0, 0, 270) && p87.rotation == Quaternion.Euler (0, 0, 180) && (p86.rotation == Quaternion.Euler (0, 0, 0) ||
+			p86.rotation == Quaternion.Euler (0, 0, 180)) && (p85.rotation == Quaternion.Euler (0, 0, 0) || p85.rotation == Quaternion.Euler (0, 0, 180)) &&
+			p84.rotation == Quaternion.Euler (0, 0, 270) && p94.rotation == Quaternion.Euler (0, 0, 180) && (p93.rotation == Quaternion.Euler (0, 0, 0) ||
+			p93.rotation == Quaternion.Euler (0, 0, 180)) && p92.rotation == Quaternion.Euler (0, 0, 0) && (p82.rotation == Quaternion.Euler (0, 0, 0) ||
+			p82.rotation == Quaternion.Euler (0, 0, 180)) && (p62.rotation == Quaternion.Euler (0, 0, 0) || p62.rotation == Quaternion.Euler (0, 0, 180)) &&
+			(p52.rotation == Quaternion.Euler (0, 0, 0) || p52.rotation == Quaternion.Euler (0, 0, 180)) && p22.rotation == Quaternion.Euler (0, 0, 270) &&
+			p23.rotation == Quaternion.Euler (0, 0, 180) && p53.rotation == Quaternion.Euler (0, 0, 0) && (p54.rotation == Quaternion.Euler (0, 0, 0) ||
+			p54.rotation == Quaternion.Euler (0, 0, 180)) && p55.rotation == Quaternion.Euler (0, 0, 180) && (p65.rotation == Quaternion.Euler (0, 0, 0) ||
+			p65.rotation == Quaternion.Euler (0, 0, 180)) && p75.rotation == Quaternion.Euler (0, 0, 180) && (p74.rotation == Quaternion.Euler (0, 0, 0) ||
+			p74.rotation == Quaternion.Euler (0, 0, 180)) && (p73.rotation == Quaternion.Euler (0, 0, 0) || p73.rotation == Quaternion.Euler (0, 0, 180)) &&
+			p71.rotation == Quaternion.Euler (0, 0, 0) && (p61.rotation == Quaternion.Euler (0, 0, 0) || p61.rotation == Quaternion.Euler (0, 0, 180)) &&
+			(p51.rotation == Quaternion.Euler (0, 0, 0) || p51.rotation == Quaternion.Euler (0, 0, 180)) && p41.rotation == Quaternion.Euler (0, 0, 270) &&
+			p44.rotation == Quaternion.Euler (0, 0, 180) && p34.rotation == Quaternion.Euler (0, 0, 180) && (p31.rotation == Quaternion.Euler (0, 0, 0) ||
+			p31.rotation == Quaternion.Euler (0, 0, 180)) && p30.rotation == Quaternion.Euler (0, 0, 270) && (p50.rotation == Quaternion.Euler (0, 0, 0) ||
+			p50.rotation == Quaternion.Euler (0, 0, 180)) && (p70.rotation == Quaternion.Euler (0, 0, 0) || p70.rotation == Quaternion.Euler (0, 0, 180)) &&
+			(p80.rotation == Quaternion.Euler (0, 0, 0) || p80.rotation == Quaternion.Euler (0, 0, 180)) && p90.rotation == Quaternion.Euler (0, 0, 180)) {
+			Debug.Log ("win");
 		}
 	
 	}
