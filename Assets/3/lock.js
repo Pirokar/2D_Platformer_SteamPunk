a5289027f34ae9f2d65185125a6e263e6dea8e30
Pirokar <Pirokar13@yandex.ru>
2013-09-01 16:20:15 +0400

    init commit

--- Assets/3/lock.js
+++
@@ -1,6 +0,0 @@
-﻿
-function OnMouseUp(){
-
-	transform.localScale.x=0.75;
-	transform.localScale.z=0.75;
-}
---
+++ Assets/ThirdLogic/lock.js
@@ -0,0 +1,13 @@
+﻿var click : boolean = false;
+
+function OnMouseUp(){
+	click = !click;
+	if(!click) {
+		transform.localScale.x=1;
+		transform.localScale.z=1;
+	}
+	else {
+		transform.localScale.x=0.75;
+		transform.localScale.z=0.75;
+	}
+}
